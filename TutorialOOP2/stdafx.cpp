9ccb137adbd9a8101613bde7e1a192abe6323d07
Anđela Todorović <andjelatodorovicc@gmail.com>
2024-07-04 13:39:33 +0200

    Delete TutorialOOP2 directory

--- TutorialOOP2/stdafx.cpp
+++
@@ -1,8 +0,0 @@
-
-// stdafx.cpp : source file that includes just the standard includes
-// TutorialOOP2.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
